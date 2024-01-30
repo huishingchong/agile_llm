8995c332f3fa9a9cd43f18146694c4ea00eb6b17
huishingchong <huishingchong@gmail.com>
2024-01-30 13:19:52 +0000

    Implement simple QA with model

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # !pip install transformers datasets torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from getpass import getpass
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from langchain import HuggingFaceHub
+from langchain.chains import LLMChain, ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate 
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -14,11 +18,18 @@ if not huggingface_api_token:
     huggingface_api_token = getpass("Enter your Hugging Face Hub API token: ")
 
 # specify HuggingFace model
-model_name = "google/flan-t5-xxl"
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+model_name = "google/flan-t5-small"
+# model_name2 = "roberta-base"
+llm = HuggingFaceHub(repo_id=model_name, model_kwargs={"temperature":0, "max_length":64})
 
 # prompting with langchain
+template = """Question: {question}
+ 
+Answer: """
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+question = "Give me the definition of cybersecurity."
+print(llm_chain.run(question))
 
 # Fine-tune LLM?
 # RAG from Wikipedia
