8978467e8cdcc034e63230191cf4a8be6588f4f3
huishingchong <huishingchong@gmail.com>
2024-02-15 22:49:49 +0000

    Add Jupyter notebook and try RAG

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# !pip install transformers datasets torch
+# !pip install transformers datasets torch pinecone-client langchain-community faiss-cpu sentence-transformers
 from getpass import getpass
 from dotenv import load_dotenv
 import os
@@ -7,9 +7,18 @@ import gradio as gr
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from datasets import load_dataset
-from langchain import HuggingFaceHub
+from langchain_community.llms import HuggingFaceHub
 from langchain.chains import LLMChain, ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+
+from pinecone import Pinecone, ServerlessSpec
+
+pc = Pinecone(api_key='YOUR_API_KEY')
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -22,57 +31,72 @@ if not huggingface_api_token:
 # specify HuggingFace model
 model_name = "google/flan-t5-xxl"
 # model_name = "tiiuae/falcon-7b"
-# model_name = "openai-community/gpt2"
-# model_name = "meta-llama/Llama-2-70b-chat-hf"
-# model_name = "databricks/dolly-v2-3b"
-llm = HuggingFaceHub(repo_id=model_name, model_kwargs={"temperature":0, "max_length":400})
+llm = HuggingFaceHub(repo_id=model_name, model_kwargs={"temperature":0.5, "max_length":1024, "max_new_tokens":200})
 
 # prompting with langchain
 
 # template = """Question: {question}
 # Answer: """
 
-instructions = """
-    Recommend topic 1
-    Put content of topic 2 here.
-    
-    Recommend topic 2
-    Put content of topic 2 here...
-    And continue for all the content you recommend for the given topic.
-"""
+# template = """
 
-example = """
-    Question: What is Cybersecurity?
-    Response: 
-    Cybersecurity is the practice of securing information technology systems and networks from cyber attacks.
-    To study Cybersecurity, you need to learn about different type of cyber attacks, for example, spoofing, phishing.
-    The current in demand market skills for cybersecurity is using anti-malware systems and batch scripting.
-"""
+# You are a content recommender for a Computer Science Online Learning platform. 
+# Your task is to suggest educational content for a Computer Science learner. Given the user
+# input prompt, generate a reply with generated topic of educational content for learner
+# to learn to help fulfil their goal. Then suggest skills to learn based on current workforce demands.
+# An example scenario is 
+#     Question: What is Cybersecurity?
+#     Response: 
+#     Cybersecurity is the practice of securing information technology systems and networks from cyber attacks.
+#     To study Cybersecurity, you need to learn about different type of cyber attacks, for example, spoofing, phishing.
+#     Examples of current in-demand skills for a career in Cybersecurity is Vulnerability Assessment and Penetration Testing (VAPT) and scripting.
+# Follow the example scenario to produce a response.
+# Make sure to use specific Computer Science terms and correct, timely, factual information in the response.
+# Question: {question}
+# Response: 
+# """
 
-template = """
+# template = """
+# You are a content recommender for a Computer Science Online Learning platform. 
+# Your task is to suggest educational content for a Computer Science learner and answer the prompt. Given the user
+# input prompt, generate a reply with generated topic of educational content for learner
+# to learn to help fulfil their goal. Then suggest skills to learn based on current workforce demands.
+# Make sure to use specific Computer Science terms and correct, timely, factual information in the response.
+# Question: {question}
+# Response: 
+# """
 
-You are a content recommender for a Computer Science Online Learning platform. 
-Your task is to suggest educational content for a Computer Science learner. Given the user
-input prompt, generate a reply with generated topic of educational content for learner
-to learn to help fulfil their goal. Then suggest skills to learn based on current workforce demands.
-An example response is 
-    Question: What is Cybersecurity?
-    Response: 
-    Cybersecurity is the practice of securing information technology systems and networks from cyber attacks.
-    To study Cybersecurity, you need to learn about different type of cyber attacks, for example, spoofing, phishing.
-    The current in demand market skills for cybersecurity is using anti-malware systems and batch scripting.
-Follow the example response.
-Make sure to use specific Computer Science terms and correct, timely, factual information in the response.
+template= """
+Try to be helpful as you can in Computer Science context.
 Question: {question}
 Response:
 """
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+# llm_chain = LLMChain(prompt=prompt, llm=llm)
+llm_chain = load_qa_chain(llm, chain_type="stuff")
+
+# RAG from synthetic data set
+loader = CSVLoader(file_path="rag_sample.csv")
+documents = loader.load() # load data for retrieval
+embeddings = HuggingFaceEmbeddings()
+text_split = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+d = text_split.split_documents(documents)
+    # https://python.langchain.com/docs/integrations/vectorstores/faiss
+db = FAISS.from_documents(d, embeddings)
+
+#USE TOKENIZER?
+#HOW TO MAKE MODEL OUTPUT MORE? CHANGE MODEL??
+#USE JUPYTER??
+#AGREE ON EVALUATION
 
 def chat_interface(textbox, chat):
-    input_dict = {'question': textbox}
+    docs = db.similarity_search(textbox)
+    input_dict = {'question': textbox, 'input_documents': docs }
     response = llm_chain.run(input_dict)
+
+    print("user:", textbox)
+    print("bot:", response)
     return response
 
 gr.ChatInterface(
@@ -90,11 +114,35 @@ gr.ChatInterface(
 ).launch()
 
 # Fine-tune LLM?
+    # Prepare dataset containing input output example types
+    # Load dataset from HuggingFace
+# dataset = load_dataset("Locutusque/UltraTextbooks")
 
-# Load dataset from HuggingFace
+# data = load_dataset("wikipedia", "20220301.simple", split='train[:10000]')
+# data = load_dataset('fever', 'wiki_pages')
+# data["train"][100]
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# def tokenize_function(examples):
+#     return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+# tokenized_datasets = data.map(tokenize_function, batched=True)
+# print(tokenized_datasets)
+
+
+# pinecone_api_key = os.getenv("PINECONE_API_KEY")
+# pc = Pinecone(api_key=api_key)
+# pc.create_index(
+#     name="quickstart",
+#     dimension=8,
+#     metric="euclidean",
+#     spec=ServerlessSpec(
+#         cloud='aws', 
+#         region='us-west-2'
+#     )
+# )
+
 
 # RAG from Wikipedia
 
-# RAG from synthetic data set
 
 # Evaluation
